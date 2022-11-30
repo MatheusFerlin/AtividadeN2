fae35d4cf3c2bf19bc3c83b129f25d47792fad82
MatheusFerlin <81568563+MatheusFerlin@users.noreply.github.com>
2022-11-29 22:02:52 -0300

    Add files via upload

--- AtividadeComputacaoGraficaMatheusFerlin/Scripts/GlobalVariable.gd
+++ AtividadeComputacaoGraficaMatheusFerlin/Scripts/GlobalVariable.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var placar1=0 
-var placar2=0
+var placar2=11
 
 func _ready():
 	
@@ -9,6 +9,43 @@ func _ready():
 func _physics_process(delta):
 	$placa1.text = str(placar1)
 	$placa2.text = str(placar2)
+	verificarSeAlterouPosicao()
+	if placar2 == 0:
+		get_tree().quit()
+	pass
+	
+		
+
+func _on_esquerda_body_entered(body):
+	placar2 = placar2 -1
+	verificarSeAlterouPosicao()
+	print("Hit")
+	pass # Replace with function body.
+
+
+func _on_direita_body_entered(body):
+	placar2 = placar2 -1
+	verificarSeAlterouPosicao()
+	print("Hit")
+	pass # Replace with function body.
+
+
+func _on_baixo_body_entered(body):
+	placar2 = placar2 -1
+	print("Hit")
+	verificarSeAlterouPosicao()
+	pass # Replace with function body.
+
+
+func _on_cima_body_entered(body):
+	placar2 = placar2 -1
+	verificarSeAlterouPosicao()
+	print("Hit")
+	
+	pass # Replace with function body.
+
+
+func verificarSeAlterouPosicao():
 	if  Input.is_action_just_pressed("ui_right"):
 		placar1= placar1+1
 	pass
@@ -23,28 +60,3 @@ func _physics_process(delta):
 	if  Input.is_action_just_pressed("ui_left"):
 		placar1= placar1+1
 	pass
-		
-
-func _on_esquerda_body_entered(body):
-	placar2 = placar2 +1
-	print("Hit")
-	pass # Replace with function body.
-
-
-func _on_direita_body_entered(body):
-	placar2 = placar2 +1
-	print("Hit")
-	pass # Replace with function body.
-
-
-func _on_baixo_body_entered(body):
-	placar2 = placar2 +1
-	print("Hit")
-	pass # Replace with function body.
-
-
-func _on_cima_body_entered(body):
-	placar2 = placar2 +1
-	print("Hit")
-	
-	pass # Replace with function body.
